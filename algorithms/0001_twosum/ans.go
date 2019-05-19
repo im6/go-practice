981a2b99c565fe6ad27f29a4b87f8ebf1588d0f1
im6 <ee2win@gmail.com>
2019-05-18 23:41:58 -0400

    add solution

--- algorithms/0001_twosum/ans.go
+++ algorithms/0001_twosum/ans.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-
+	ref := make(map[int]int)
+	for i, v := range nums {
+		if j, ok := ref[target-v]; ok {
+			return []int{j, i}
+		}
+		ref[v] = i
+	}
+	return []int{-1, -1}
 }
 
 func main() {
-	fmt.Printf("hello, world\n")
+	result := twoSum([]int{3, 2, 4}, 6)
+	fmt.Println(result)
 }
